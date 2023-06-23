fbfe56ba8342e04fbc0edb6d331335ba1ee05524
David-McCaig <davidmccaig1@gmail.com>
2023-06-23 20:05:38 +1200

    Add polyline to google maps

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
 import React from 'react'
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Polyline } from '@react-google-maps/api';
+import { useEffect } from 'react';
+import jsonData from "./testData.json"
+import gpxData from "./gpx/lord-of-the-possums.gpx"
+// var xml2js = require('xml2js');
+import gpxParser from "gpxparser"
 
-const containerStyle = {
-  margin: "auto",
-  width: '700px',
-  height: '700px'
-};
-
-const center = {
-  lat: -43.59909,
-  lng: 172.63241
-};
 
 function App() {
+
+  var gpx = new gpxParser()
+const test = gpx.parse(gpxData)
+
+console.log(test)
+
+
+  const containerStyle = {
+    margin: "auto",
+    width: '700px',
+    height: '700px'
+  };
+  
+  const center = {
+    lat: -43.59909,
+    lng: 172.63241
+  };
+
+
+    const flightPlanCoordinates = [
+      { lat: -43.59909, lng: 172.63241 },
+      { lat: -43.69909, lng: 172.63241 },
+    ];
+ 
+
+
+  // const flightPath = new Polyline({
+  //   path: flightPlanCoordinates,
+  //   geodesic: true,
+  //   strokeColor: "#FF0000",
+  //   strokeOpacity: 1.0,
+  //   strokeWeight: 2,
+  // });
+
+  // useEffect(() => {
+
+  //   parseString(gpxData, function (err, result) {
+  //     console.dir(result);
+  // });
+
+  // },[])
+
   return (
     <LoadScript
       googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAP_KEY}
@@ -23,11 +60,16 @@ function App() {
         zoom={15}
       >
         { /* Child components, such as markers, info windows, etc. */ }
+        <Polyline
+              path={flightPlanCoordinates}
+              strokeColor="#FF0000"
+              strokeOpacity={1}
+              strokeWeight={2} />
         <></>
       </GoogleMap>
     </LoadScript>
   )
 }
 
-export default React.memo(App)
+export default App
 
